0bd6b9ebffe4526f4f6e5593dd4302c31d0a3c4f
TiclosOlimpia <olimpiaticlos@yahoo.com>
2018-03-18 16:14:07 +0200

    Add files via upload

--- sendemail.php
+++ sendemail.php
@@ -1,25 +1,11 @@
 <?php
-$name       = @trim(stripslashes($_POST['name'])); 
-$from       = @trim(stripslashes($_POST['email'])); 
-$telefon    = @trim(stripslashes($_POST['telefon'])); 
-$message    = @trim(stripslashes($_POST['mesaj'])); 
-
-$email_from = $email;
-$to   		= 'ghergheluca_edi@yahoo.com';//replace with your email
-
-
-$headers[] = "MIME-Version: 1.0";
-$headers[] = "Content-type: text/plain; charset=iso-8859-1";
-$headers[] = "From: $name";
-
-$headers[] = "Telefon: $telefon";
-$headers[] = "X-Mailer: PHP/".phpversion();
-
-
-mail($to, $telefon, "Mesaj de la $from: \n".  $message, implode("\r\n", $headers));
-
-var_dump($_POST);
-var_dump("From: $name <$from>");
+$name = $_POST['name'];
+$subject = $_POST['subject'];
+$email = $_POST['email'];
+$message = $_POST['mesaj'];
+$to = "ghergheluca_edi@yahoo.com";
+$message = wordwrap($msg,70);
+mail($to, $subject, $message);
 
 die;
 ?>
