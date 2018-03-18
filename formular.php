5853c6e4bf05744bfe196612339ea231137d3c0b
alamasanute <almmondducherr@yahoo.com>
2018-03-18 12:47:38 +0200

    Add files via upload

--- formular.php
+++ formular.php
@@ -6,6 +6,19 @@ $var3 = $_POST['password'];
 
 $file=fopen("mytext.txt","a+");
 
+if(isset($_POST['profesor'])) {
+    fwrite($file, "Profesie: ");    
+   fwrite($file,"profesor");
+   fwrite($file, "\r\n");
+}
+
+
+if(isset($_POST['student'])){
+    fwrite($file, "Profesie: ");    
+   fwrite($file,"student");
+   fwrite($file, "\r\n");
+}
+
 if($var1 <> "" and $var2 <> "" and $var3 <> "") {
     fwrite($file, "Nume: ");    
    fwrite($file,$var1);
